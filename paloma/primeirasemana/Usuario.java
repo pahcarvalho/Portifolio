4f73c28584d62bb13e490437fa3a9ee658632976
Paloma Lima <paloma.clima@gmail.com>
2023-10-14 13:00:59 -0400

    SmartTV - Métodos

--- paloma/primeirasemana/Usuario.java
+++ paloma/primeirasemana/Usuario.java
@@ -1,12 +1,23 @@
 public class Usuario {
      public static void main (String[] args)throws Exception{
       
-        SmartTv smartTv = new smartTv();
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        System.out.println("O volume atual é " + smartTv.volume);
+
+        smartTv.diminuirVolume();
+        System.out.println("O volume atual é " + smartTv.volume);
+
+
 
         System.out.println("TV ligada?" + smartTv.ligada);
         System.out.println("Qual o canal atual?" + smartTv.canal);
         System.out.println("Qual o volume atual?" + smartTv.volume);
         
+        smartTv.ligar();
+        System.out.println("Novo status - : TV ligada?" + smartTv.ligada);
 
     }
 
